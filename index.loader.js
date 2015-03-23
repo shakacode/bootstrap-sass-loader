4e42e5f4ae98566037f9d7b8929183def0d18213
Justin Gordon <justin.gordon@gmail.com>
2015-03-22 17:31:23 -1000

    Updated getting resource name.
    * Newer versions of Webpack require the user of this.resourcePath inside
      of the module.exports.pitch function.

--- index.loader.js
+++ index.loader.js
@@ -5,13 +5,17 @@ module.exports = function (content) {
 
 module.exports.pitch = function (remainingRequest) {
   this.cacheable(true);
-  var configFilePath = remainingRequest;
+
+  // Webpack 1.7.3 uses this.resourcePath. Leaving in remaining request for possibly older versions of Webpack
+  var configFilePath = this.resourcePath || remainingRequest;
+
   if (!configFilePath || configFilePath == null || configFilePath == "") {
     var msg = "You specified the bootstrap-sass-loader with no configuration file. Please specify the configuration "
       + "file, like: 'bootstrap-sass!./bootstrap-sass.config.js' or use require('bootstrap-sass-loader').";
     console.error("ERROR: " + msg);
     throw new Error(msg);
   }
+  
   var config = require(configFilePath);
   var styleLoader = config.styleLoader || "style-loader!css-loader!sass-loader";
   logger.verbose(config, "styleLoader: %s", styleLoader);
